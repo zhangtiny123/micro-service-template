58f7389e13c7b15c85bbebe759ed6dfe3068b111
zhangtiny123 <tzhang920314@gmail.com>
2015-03-25 13:56:42 +0800

    add test of health diagnostics controller

--- src/main/java/com/example/template/Application.java
+++ src/main/java/com/example/template/Application.java
@@ -2,10 +2,7 @@ package com.example.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +16,5 @@ public class Application
     public static void main(String[] args) throws Exception
     {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping(value = "/appCheck", method = RequestMethod.GET)
-    public ResponseEntity<String> helloWorld()
-    {
-
-        return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
     }
 }
---
+++ src/test/java/com/example/template/controllers/DiagnosticsTest.java
@@ -0,0 +1,35 @@
+package com.example.template.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:ApplicationContext.xml"})
+public class DiagnosticsTest
+{
+    @Autowired
+    private Diagnostics diagnostics;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        mockMvc = MockMvcBuilders.standaloneSetup(diagnostics).build();
+    }
+
+    @Test
+    public void should_return_dependencies_list() throws Exception
+    {
+        mockMvc.perform(get("/dependencies")).andExpect(status().isOk());
+    }
+}
